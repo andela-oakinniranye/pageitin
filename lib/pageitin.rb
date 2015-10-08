92af93eaa84b33bf9c6cf502c201930336e700f8
Oreoluwa Akinniranye <oreoluwa.akinniranye@andela.com>
2015-10-08 10:27:15 +0100

    started refactoring process

--- lib/pageitin.rb
+++ lib/pageitin.rb
@@ -1,5 +1,6 @@
 require "pageitin/version"
 require "active_record"
+require "active_support/all"
 require 'pageitin/paginate'
 
 module Pageitin
--- lib/pageitin/paginate.rb
+++ lib/pageitin/paginate.rb
@@ -1,20 +1,30 @@
 module Pageitin
-  module Paginate
-    class ::ActiveRecord::Base
-      def self.paginate(page: 1, per_page: 10)
-        @@per_page = per_page
-        offset_param = (page * per_page) - per_page
-        limit(per_page).offset(offset_param)
-      end
-
-      def config(*args)
-        #set default values
-      end
-
-      def self.total_page
-        (count.to_f / @@per_page).ceil
-      end
+  extend ActiveSupport::Concern
+  module ClassMethods
+    def paginate(page: 1, per_page: 10)
+      offset_param = (page * per_page) - per_page
+      limit(per_page).offset(offset_param)
     end
+
+    def total_page(per_page)
+      (count.to_f / per_page).ceil
+    end
+  end
+
+  module InstanceMethods
 
   end
 end
+
+ActiveRecord::Base.include Pageitin
+
+    # cattr
+      # per_page = per_page
+
+    # def config(*args)
+    #   #set default values
+    # end
+
+# class ActiveRecord::Base
+#   include Pageitin
+# end
